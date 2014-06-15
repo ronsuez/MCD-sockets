3e2c4c4ff61afdbcdfb166f334921c4d5336c2eb
anita <anita@anita-ThinkPad-SL>
2014-06-15 03:55:29 -0430

    Subí la estructura del proyecto

---
+++ mcd-sockets/main.c
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.c
+ * Author: anita
+ *
+ * Created on 15 de junio de 2014, 03:52 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    printf("Hello world");
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ mcd-sockets/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=mcd-sockets
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/mcd-sockets
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=mcd-sockets.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/mcd-sockets.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=mcd-sockets
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/mcd-sockets
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=mcd-sockets.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/mcd-sockets.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
